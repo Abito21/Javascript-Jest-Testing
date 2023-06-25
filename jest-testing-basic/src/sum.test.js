fe9997370fc0a011c3d11826c133972b726274f0
Abito21 <abidjuliant@gmail.com>
2023-06-25 17:09:50 +0700

    Test babel config js

--- jest-testing-basic/src/sum.test.js
+++ jest-testing-basic/src/sum.test.js
@@ -1,7 +1,7 @@
 import add from './sum';
 
-escribe('sum.js', function() {
+describe('sum.js', function() {
     test('add two numbers', function(){
-        expect(add(1, 2)). toBe(3);
-    })
-})
+        expect(add(1, 2)).toBe(3);
+    });
+});
