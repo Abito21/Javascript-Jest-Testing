a8c7d8a7a26b30c681bf24de9b7196b500da5cc2
Abito21 <abidjuliant@gmail.com>
2023-06-26 20:15:09 +0700

    Update Script Test

--- jest-testing-basic/src/sum.test.js
+++ jest-testing-basic/src/sum.test.js
@@ -1,7 +1,11 @@
+import sub from './substraction';
 import add from './sum';
 
-describe('sum.js', function() {
+describe('Testing fungsi matematis', function() {
     test('add two numbers', function(){
         expect(add(1, 2)).toBe(3);
+    });
+    test('substraction two numbers', function(){
+        expect(sub(2, 1)).toBe(1);
     });
 });
