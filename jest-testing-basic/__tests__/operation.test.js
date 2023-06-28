89c3f4b58bf1b01eec3bf0eb87ab1128e6c3689c
Abito21 <abidjuliant@gmail.com>
2023-06-28 11:16:28 +0700

    Update test case format name

--- jest-testing-basic/__tests__/operation.test.js
+++ jest-testing-basic/__tests__/operation.test.js
@@ -4,16 +4,16 @@ import add from '../src/addition';
 import div from '../src/division';
 
 describe('Testing Mathemathic Function', function() {
-    test('add two numbers', function(){
+    test('Testing function add two numbers', function(){
         expect(add(1, 2)).toBe(3);
     });
-    test('substraction two numbers', function(){
+    test('Testing function substraction two numbers', function(){
         expect(sub(2, 1)).toBe(1);
     });
-    test('multiplication two numbers', function(){
+    test('Testing function multiplication two numbers', function(){
         expect(mul(2, 1)).toBe(2);
     });
-    test('multiplication two numbers', function(){
+    test('Testing function multiplication two numbers', function(){
         expect(div(2, 1)).toBe(2);
     });
 });
