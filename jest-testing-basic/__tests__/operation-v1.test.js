9bd8310b9dfb197dcff20abd972d40ead58829a0
Abito21 <abidjuliant@gmail.com>
2023-06-29 19:05:26 +0700

    Update test script

--- jest-testing-basic/__tests__/operation-v1.test.js
+++ jest-testing-basic/__tests__/operation-v1.test.js
@@ -1,19 +1,38 @@
-import mul from '../src/multiplication';
-import sub from '../src/substraction';
 import add from '../src/addition';
 import div from '../src/division';
+import mul from '../src/multiplication';
+import sub from '../src/substraction';
 
-describe('Testing Mathemathic Function', function() {
-    test('Testing function add two numbers', function(){
-        expect(add(1, 2)).toBe(3);
+// Declare Variable
+let resultAdditionPembanding;
+let resultDivisiontionPembanding;
+let resultMultiplicationPembanding;
+let resultSubstractionPembanding;
+
+describe('Testing Mathemathic Function', () => {
+    beforeAll(async () => {
+        // Funtion Addition
+        resultAdditionPembanding = add(2, 1);
+        
+        // Funtion Addition
+        resultDivisiontionPembanding = div(2, 1);
+        
+        // Funtion Addition
+        resultMultiplicationPembanding = mul(2, 1);
+
+        // Funtion Addition
+        resultSubstractionPembanding = sub(2, 1);
     });
-    test('Testing function substraction two numbers', function(){
-        expect(sub(2, 1)).toBe(1);
+    test('Testing function add two numbers', async () => {
+        expect(add(1, 2)).toBe(resultAdditionPembanding);
     });
-    test('Testing function multiplication two numbers', function(){
-        expect(mul(2, 1)).toBe(2);
+    test('Testing function division two numbers', async () => {
+        expect(div(2, 1)).toBe(resultDivisiontionPembanding);
     });
-    test('Testing function multiplication two numbers', function(){
-        expect(div(2, 1)).toBe(2);
+    test('Testing function multiplication two numbers', async () => {
+        expect(mul(2, 1)).toBe(resultMultiplicationPembanding);
+    });
+    test('Testing function substraction two numbers', async () => {
+        expect(sub(2, 1)).toBe(resultSubstractionPembanding);
     });
 });
