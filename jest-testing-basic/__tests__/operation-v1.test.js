d6c99f6f48cda6cb57d01679a14d8845cfca08f5
Abito21 <abidjuliant@gmail.com>
2023-06-30 07:11:21 +0700

    Update Testing Script Function Pembanding

--- jest-testing-basic/__tests__/operation-v1.test.js
+++ jest-testing-basic/__tests__/operation-v1.test.js
@@ -4,35 +4,51 @@ import mul from '../src/multiplication';
 import sub from '../src/substraction';
 
 // Declare Variable
+let resultAddition;
 let resultAdditionPembanding;
-let resultDivisiontionPembanding;
+let resultDivision;
+let resultDivisionPembanding;
+let resultMultiplication;
 let resultMultiplicationPembanding;
+let resultSubstraction;
 let resultSubstractionPembanding;
 
 describe('Testing Mathemathic Function', () => {
     beforeAll(async () => {
         // Funtion Addition
+        resultAddition = add(2, 1);
+        
+        // Funtion Addition Pembanding
         resultAdditionPembanding = add(2, 1);
         
-        // Funtion Addition
-        resultDivisiontionPembanding = div(2, 1);
+        // Funtion Division
+        resultDivision= div(2, 1);
+
+        // Funtion Division Pembanding
+        resultDivisionPembanding = div(2, 1);
         
-        // Funtion Addition
+        // Funtion Multiplication
+        resultMultiplication = mul(2, 1)
+        
+        // Funtion Multiplication Pembanding
         resultMultiplicationPembanding = mul(2, 1);
 
-        // Funtion Addition
+        // Funtion Substraction
+        resultSubstraction = sub(2, 1);
+
+        // Funtion Substraction Pembanding
         resultSubstractionPembanding = sub(2, 1);
     });
     test('Testing function add two numbers', async () => {
-        expect(add(1, 2)).toBe(resultAdditionPembanding);
+        expect(resultAddition).toBe(resultAdditionPembanding);
     });
     test('Testing function division two numbers', async () => {
-        expect(div(2, 1)).toBe(resultDivisiontionPembanding);
+        expect(resultDivision).toBe(resultDivisionPembanding);
     });
     test('Testing function multiplication two numbers', async () => {
-        expect(mul(2, 1)).toBe(resultMultiplicationPembanding);
+        expect(resultMultiplication).toBe(resultMultiplicationPembanding);
     });
     test('Testing function substraction two numbers', async () => {
-        expect(sub(2, 1)).toBe(resultSubstractionPembanding);
+        expect(resultSubstraction).toBe(resultSubstractionPembanding);
     });
 });
