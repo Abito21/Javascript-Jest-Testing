e1605735405fb10e4cd577ceb0577659e0a343e1
Abito21 <abidjuliant@gmail.com>
2023-07-05 11:07:00 +0700

    Update Script Compare String Function

--- jest-testing-data/__tests__/compareString.test.js
+++ jest-testing-data/__tests__/compareString.test.js
@@ -1,7 +1,23 @@
-import add from "../data/compareString";
+import add from '../data/compareString';
+import * as base from '../common/baseFunction';
 
-describe('Testing fungsi penambahan', function() {
-    test('Fungsi Penambahan', function() {
+const txtToJson = require("txt-file-to-json");
+
+// Variable Declare
+let stringData;
+let stringDataPembanding;
+
+describe('Testing fungsi penambahan', () => {
+    beforeAll(async () => {
+        // File String Data
+        stringData = txtToJson({ filePath: "./data/compareString.txt" });
+        console.log(stringData);
+
+        // Comparison File String Data 
+        stringDataPembanding = txtToJson({ filePath: "./data/compareStringPembanding.txt" });
+        console.log(stringDataPembanding);
+    });
+    test('Fungsi Penambahan', async () => {
         expect(add(1, 2)).toBe(3);
     });
 });
