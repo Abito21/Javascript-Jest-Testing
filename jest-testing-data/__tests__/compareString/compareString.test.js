db279047fc021cde945ddbba32c45b933c4ca32f
Abito21 <abidjuliant@gmail.com>
2023-07-07 16:31:44 +0700

    Update compareString Test file

--- jest-testing-data/__tests__/compareString/compareString.test.js
+++ jest-testing-data/__tests__/compareString/compareString.test.js
@@ -1,5 +1,5 @@
-import * as column from '../data/compareString'
-import * as base from '../common/baseFunction';
+import * as column from '../../data/compareString/compareString'
+import * as base from '../../common/baseFunction';
 
 const txtToJson = require("txt-file-to-json");
 
@@ -10,12 +10,12 @@ let stringDataPembanding;
 describe('Testing fungsi penambahan', () => {
     beforeAll(async () => {
         // File String Data
-        stringDataTarget = txtToJson({ filePath: "./data/compareString.txt" });
+        stringDataTarget = txtToJson({ filePath: "./data/compareString/compareStringTarget.txt" });
         stringDataTarget = base.addCombinedKey(stringDataTarget);
         console.log(stringDataTarget);
 
         // Comparison File String Data 
-        stringDataPembanding = txtToJson({ filePath: "./data/compareStringPembanding.txt" });
+        stringDataPembanding = txtToJson({ filePath: "./data/compareString/compareStringPembanding.txt" });
         stringDataPembanding = base.addCombinedKey(stringDataPembanding);
         console.log(stringDataPembanding);
     });
