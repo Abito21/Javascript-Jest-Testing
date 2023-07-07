1a735f2e8dd84c480be7ed3a873753b2fda51ae7
Abito21 <abidjuliant@gmail.com>
2023-07-07 16:28:16 +0700

    Create example.test.js

---
+++ jest-testing-data/__tests__/example.test.js
@@ -0,0 +1,25 @@
+import * as column from '../data/compareString/compareString'
+import * as base from '../common/baseFunction';
+
+const txtToJson = require("txt-file-to-json");
+
+// Variable Declare
+let stringDataTarget;
+let stringDataPembanding;
+
+describe('Testing fungsi penambahan', () => {
+    beforeAll(async () => {
+        // File String Data
+        stringDataTarget = txtToJson({ filePath: "./data/compareString/compareStringTarget.txt" });
+        stringDataTarget = base.addCombinedKey(stringDataTarget);
+        console.log(stringDataTarget);
+
+        // Comparison File String Data 
+        stringDataPembanding = txtToJson({ filePath: "./data/compareString/compareStringPembanding.txt" });
+        stringDataPembanding = base.addCombinedKey(stringDataPembanding);
+        console.log(stringDataPembanding);
+    });
+    test('Testing detail data compareString ', async () => {
+        base.compareValue(stringDataTarget, stringDataPembanding, column.targetTestColumn);
+    });
+});
